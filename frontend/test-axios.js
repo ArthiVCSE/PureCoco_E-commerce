cf447afe2c045d2e455fa16fcf8d1db3e9b192eb
ArthiVCSE <arthi.v2024lcse@sece.ac.in>
2026-06-17 23:07:12 +0530

    test api

---
+++ frontend/test-axios.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const api = axios.create({
+  baseURL: 'https://purecoco-e-commerce.onrender.com/api',
+});
+
+api.interceptors.request.use(config => {
+  console.log('Full URL will be:', axios.getUri(config));
+  return config;
+});
+
+api.post('/auth/register').catch(() => {});
